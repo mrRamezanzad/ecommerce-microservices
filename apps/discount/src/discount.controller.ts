ccc3d0450274a9739b386925ecdc976973f2a8e5
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-03 13:35:14 +0330

    feat(coupon): add getDiscount method in proper controller and service

--- apps/discount/src/discount.controller.ts
+++ apps/discount/src/discount.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { DiscountService } from './discount.service';
+import { ICoupon } from './entity/coupon.entity.interface';
 
-@Controller()
+@Controller('discounts')
 export class DiscountController {
   constructor(private readonly discountService: DiscountService) {}
 
-  @Get()
-  getHello(): string {
-    return this.discountService.getHello();
+  @Get(':productName')
+  getDiscount(@Param('productName') productName: string): Promise<ICoupon> {
+    return this.discountService.getDiscount(productName);
   }
 }
--- apps/discount/src/discount.service.ts
+++ apps/discount/src/discount.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ICoupon } from './entity/coupon.entity.interface';
+import { ICouponRepository } from './repository/coupon.repository.interface';
 
 @Injectable()
 export class DiscountService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly couponRepository: ICouponRepository) {}
+
+  async getDiscount(productName: string): Promise<ICoupon> {
+    return this.couponRepository.getDiscount(productName);
   }
 }
