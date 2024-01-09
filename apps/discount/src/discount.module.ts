333f5355408df70b8818b3a20704a6d5c28a1aed
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-09 09:06:54 +0330

    feat: add mikro-orm setup to project

--- apps/discount/src/discount.module.ts
+++ apps/discount/src/discount.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DiscountController } from './discount.controller';
 import { DiscountService } from './discount.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      autoLoadEntities: true,
+      dbName: process.env.DATABASE_NAME,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      type: 'postgresql',
+      discovery: { warnWhenNoEntities: false }
+    }),
   controllers: [DiscountController],
   providers: [DiscountService],
 })
--- apps/discount/src/main.ts
+++ apps/discount/src/main.ts
@@ -2,9 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { DiscountModule } from './discount.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
+import { MikroORM } from '@mikro-orm/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(DiscountModule);
+  const schemaGenerator = app.get(MikroORM).getSchemaGenerator()
+
+  await schemaGenerator.ensureDatabase();
+
+  const isProduction = process.env.NODE_ENV === "production"
+  if (!isProduction) {
+    await schemaGenerator.dropSchema();
+    await schemaGenerator.createSchema();
+
+  } else {
+    Logger.warn(await schemaGenerator.getUpdateSchemaSQL({
+      safe: true
+    }), 'Database Migration');
+  }
+
   const config = new DocumentBuilder()
     .setTitle('Discount API')
     .setDescription('Thiss documentation shows how to do CRUD on the coupons.')
