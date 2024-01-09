a394b604a3921b676bb26251c6b45c90e3882df6
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-09 13:06:54 +0330

    feat: add id to updateDiscountDto

--- apps/discount/src/dtos/updateDiscount.dto.ts
+++ apps/discount/src/dtos/updateDiscount.dto.ts
@@ -1,5 +1,8 @@
-import { PartialType } from "@nestjs/swagger";
-import { CreateDiscountDto } from "./createDiscount.dto";
+import { PartialType } from '@nestjs/swagger';
+import { CreateDiscountDto } from './createDiscount.dto';
+import { IsString } from '@nestjs/class-validator';
 
 export class UpdateDiscountDto extends PartialType(CreateDiscountDto) {
+  @IsString()
+  id: string;
 }
