d8e028f9353d7162952f37627041765bb1cf1fd4
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-09 13:06:15 +0330

    feat(coupon) : make productName of coupon entity unique

--- apps/discount/src/entity/coupon.entity.ts
+++ apps/discount/src/entity/coupon.entity.ts
@@ -1,30 +1,35 @@
-import { Entity, FloatType, PrimaryKey, Property, StringType } from '@mikro-orm/core';
+import {
+  Entity,
+  FloatType,
+  PrimaryKey,
+  Property,
+  StringType,
+  Unique,
+} from '@mikro-orm/core';
 import { ICoupon } from './coupon.entity.interface';
 
-@Entity(
-  {
-    tableName: 'coupons'
-  }
-)
+@Entity({
+  tableName: 'coupons',
+})
 export class Coupon implements ICoupon {
   @PrimaryKey({ autoincrement: true, type: 'varchar' })
   public id: string;
 
   @Property({
-    type: StringType
+    type: StringType,
   })
-  public productName: string
+  @Unique()
+  public productName: string;
 
   @Property({
-    type: StringType
+    type: StringType,
   })
-  public description: string
+  public description: string;
 
   @Property({
-    type: FloatType
-
+    type: FloatType,
   })
-  public amount: number
+  public amount: number;
 
   constructor(productName: string, description: string, amount: number) {
     this.productName = productName;
