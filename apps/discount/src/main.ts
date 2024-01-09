b5ee45a816e8a7215f85d1a52634ff011e7811da
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-09 08:14:00 +0330

    doc: add swagger plugin and it's configs

--- apps/discount/src/main.ts
+++ apps/discount/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { DiscountModule } from './discount.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(DiscountModule);
+  const config = new DocumentBuilder()
+    .setTitle('Discount API')
+    .setDescription('Thiss documentation shows how to do CRUD on the coupons.')
+    .setVersion('1.0')
+    .addTag('discount')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  Logger.log(`Documentation is running on: ${await app.getUrl()}/api`)
 }
 bootstrap();
