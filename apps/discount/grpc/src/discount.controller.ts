bc37012e3be37482f182c506ae425e136f0f781d
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-10 20:45:57 +0330

    feat(discount.grpc): add grpc server setup and start implementing related discount rpc methods in it's controller

--- apps/discount/grpc/src/discount.controller.ts
+++ apps/discount/grpc/src/discount.controller.ts
@@ -1,12 +1,36 @@
 import { Controller, Get } from '@nestjs/common';
 import { DiscountService } from './discount.service';
+import {
+  Coupon,
+  CreateDiscountRequest,
+  DeleteDiscountRequest,
+  DeleteDiscountResponse,
+  DiscountServiceController,
+  DiscountServiceControllerMethods,
+  GetDiscountRequest,
+  UpdateDiscountRequest,
+} from '@app/common';
 
 @Controller()
-export class DiscountController {
+@DiscountServiceControllerMethods()
+export class DiscountController implements DiscountServiceController {
   constructor(private readonly discountService: DiscountService) {}
 
-  @Get()
-  getHello(): string {
-    return this.discountService.getHello();
+  getDiscount(request: GetDiscountRequest): Coupon | Promise<Coupon> {
+    throw new Error('Method not implemented.');
+  }
+
+  createDiscount(request: CreateDiscountRequest): Coupon | Promise<Coupon> {
+    throw new Error('Method not implemented.');
+  }
+
+  updateDiscount(request: UpdateDiscountRequest): Coupon | Promise<Coupon> {
+    throw new Error('Method not implemented.');
+  }
+
+  deleteDiscount(
+    request: DeleteDiscountRequest,
+  ): DeleteDiscountResponse | Promise<DeleteDiscountResponse> {
+    throw new Error('Method not implemented.');
   }
 }
--- apps/discount/grpc/src/main.ts
+++ apps/discount/grpc/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { DiscountModule } from './discount.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { DISCOUNT_PACKAGE_NAME } from '@app/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(DiscountModule);
-  await app.listen(3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    DiscountModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: DISCOUNT_PACKAGE_NAME,
+        protoPath: join(__dirname, '../discount.proto'),
+      },
+    },
+  );
+  await app.listen();
+  Logger.log(`Grpc serer started at http://[::1]:5000`, 'NestMicroservice');
 }
 bootstrap();
