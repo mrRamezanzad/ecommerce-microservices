0238dc769d9d1ae7f1ea6d45f7a39c9843abb64b
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-11 14:30:01 +0330

    feat(discount.grpc): add completed discount service

--- apps/discount/grpc/src/discount.service.ts
+++ apps/discount/grpc/src/discount.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Coupon } from './entity/coupon.entity';
+import { CouponRepository } from './repository/coupon.repository';
 
 @Injectable()
 export class DiscountService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly couponRepository: CouponRepository) {}
+
+  async create(coupon: Partial<Coupon>): Promise<Coupon> {
+    return this.couponRepository.createDiscount(coupon);
+  }
+
+  async getDiscount(productName: string): Promise<Coupon> {
+    return this.couponRepository.getDiscount(productName);
+  }
+
+  async updateDiscount(coupon: Partial<Coupon>): Promise<Coupon> {
+    return this.couponRepository.updateDiscount(coupon);
+  }
+
+  async deleteDiscount(productName: string): Promise<boolean> {
+    return this.couponRepository.deleteDiscount(productName);
   }
 }
