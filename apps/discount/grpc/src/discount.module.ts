2568c45ce23f7a9cb2d4e9f5954fecac7dc33934
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-11 14:30:34 +0330

    feat(discount.grpc): add requirements to discount module

--- apps/discount/grpc/src/discount.module.ts
+++ apps/discount/grpc/src/discount.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { DiscountController } from './discount.controller';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ConfigModule } from '@nestjs/config';
+import { Coupon } from './entity/coupon.entity';
+import { CouponRepository } from './repository/coupon.repository';
 import { DiscountService } from './discount.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MikroOrmModule.forRoot({
+      type: 'postgresql',
+      dbName: process.env.DATABASE_NAME,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      discovery: { warnWhenNoEntities: false },
+    }),
+    MikroOrmModule.forFeature({
+      entities: [Coupon],
+    }),
+  ],
   controllers: [DiscountController],
-  providers: [DiscountService],
+  providers: [CouponRepository, DiscountService],
 })
 export class DiscountModule {}
