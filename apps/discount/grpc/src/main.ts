2b2680817a201173ee3eaef0bea8a7b8898bd715
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-11 14:29:07 +0330

    feat(discount.grpc): add mikro-orm schema initializations

--- apps/discount/grpc/src/main.ts
+++ apps/discount/grpc/src/main.ts
@@ -4,6 +4,7 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { DISCOUNT_PACKAGE_NAME } from '@app/common';
 import { Logger } from '@nestjs/common';
+import { MikroORM } from '@mikro-orm/core';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -16,6 +17,26 @@ async function bootstrap() {
       },
     },
   );
+  const schemaGenerator = app.get(MikroORM).getSchemaGenerator();
+
+  await schemaGenerator.ensureDatabase();
+
+  const isProduction = process.env.NODE_ENV === 'production';
+  if (!isProduction) {
+    await schemaGenerator.dropSchema();
+    Logger.warn('Schema dropped', 'Database');
+
+    await schemaGenerator.createSchema();
+    Logger.warn('Schema created', 'Database');
+  } else {
+    Logger.warn(
+      await schemaGenerator.getUpdateSchemaSQL({
+        safe: true,
+      }),
+      'Database Migration',
+    );
+  }
+
   await app.listen();
   Logger.log(`Grpc serer started at http://[::1]:5000`, 'NestMicroservice');
 }
