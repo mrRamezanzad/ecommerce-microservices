eff5eeab3481b3024ff7a3a5f140e0e7c2c896cb
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-13 17:58:45 +0330

    refactor(catalog): modify to correct mongo repository for product

--- apps/catalog/src/repository/product.repository.ts
+++ apps/catalog/src/repository/product.repository.ts
@@ -3,7 +3,7 @@ import { Product } from '../schema/product.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
-export class ProductRespository<Product> extends MongoRepository<Product> { 
+export class ProductRespository extends MongoRepository<Product> {
   constructor(@InjectModel(Product.name) productModel: Model<Product>) {
     super(productModel);
   }
