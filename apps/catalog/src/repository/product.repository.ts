875c5772a19591b49f9221856b13b2927fcd7140
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-13 11:20:23 +0330

    feat(catalog): add product repository

---
+++ apps/catalog/src/repository/product.repository.ts
@@ -0,0 +1,10 @@
+import { MongoRepository } from './mongo.repository';
+import { Product } from '../schema/product.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+export class ProductRespository<Product> extends MongoRepository<Product> { 
+  constructor(@InjectModel(Product.name) productModel: Model<Product>) {
+    super(productModel);
+  }
+}
