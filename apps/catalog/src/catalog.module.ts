eee3f429ed1b2574b7bb1269b3c1dbc684490698
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-13 11:19:07 +0330

    feat(catalog): add mongo db configs and requirements to project

--- apps/catalog/src/catalog.module.ts
+++ apps/catalog/src/catalog.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CatalogController } from './catalog.controller';
 import { CatalogService } from './catalog.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_ADDRESS}:${process.env.MONGO_PORT}/${process.env.DATABASE_NAME}`,
+      {
+        user: process.env.MONGO_USERNAME,
+        pass: process.env.MONGO_PASSWORD,
+        authSource: 'admin',
+      },
+    ),
+  ],
   controllers: [CatalogController],
   providers: [CatalogService],
 })
