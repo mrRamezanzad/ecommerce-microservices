1890e83ceaa9df0ece35f6f259929aabb65614c2
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-22 18:48:34 +0330

    feat(catalog): complete catalog API's

--- apps/catalog/src/catalog.controller.ts
+++ apps/catalog/src/catalog.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CatalogService } from './catalog.service';
 import { Product } from './schema/product.schema';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from './dtos/createProduct.dto';
+import { UpdateProductDto } from './dtos/updateProduct.dto';
 
 @ApiTags('catalog')
 @Controller('catalog')
@@ -17,5 +26,33 @@ export class CatalogController {
   @Get()
   getProducts(): Promise<Product[]> {
     return this.catalogService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') id: string): Promise<Product> {
+    return this.catalogService.getProduct(id);
+  }
+
+  @Get('productName/:name')
+  getProductByName(@Param('name') name: string): Promise<Product> {
+    return this.catalogService.getProductByName(name);
+  }
+
+  @Get('productCategory/:category')
+  getProductByCategory(@Param('category') category: string): Promise<Product> {
+    return this.catalogService.getProductByCategory(category);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ): Promise<boolean> {
+    return this.catalogService.updateProduct(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string): Promise<boolean> {
+    return this.catalogService.deleteProduct(id);
   }
 }
--- apps/catalog/src/catalog.service.ts
+++ apps/catalog/src/catalog.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ProductRespository } from './repository/product.repository';
 import { Product } from './schema/product.schema';
 import { CreateProductDto } from './dtos/createProduct.dto';
+import { Types } from 'mongoose';
+import { UpdateProductDto } from './dtos/updateProduct.dto';
 
 @Injectable()
 export class CatalogService {
@@ -13,5 +15,46 @@ export class CatalogService {
 
   getProducts(): Promise<Product[]> {
     return this.productRepository.findAll();
+  }
+
+  getProduct(id: string): Promise<Product> {
+    return this.productRepository.findOne({
+      _id: new Types.ObjectId(id),
+    });
+  }
+
+  getProductByName(name: string): Promise<Product> {
+    return this.productRepository.findOne({
+      name,
+    });
+  }
+
+  getProductByCategory(category: string): Promise<Product> {
+    return this.productRepository.findOne({
+      category,
+    });
+  }
+
+  async updateProduct(
+    id: string,
+    updateProductDto: UpdateProductDto,
+  ): Promise<boolean> {
+    const result = await this.productRepository.updateOne(
+      {
+        _id: new Types.ObjectId(id),
+      },
+      {
+        $set: updateProductDto,
+      },
+    );
+
+    return result.modifiedCount > 0;
+  }
+
+  async deleteProduct(id: string): Promise<boolean> {
+    const result = await this.productRepository.deleteOne({
+      _id: new Types.ObjectId(id),
+    });
+    return result.deletedCount === 1;
   }
 }
