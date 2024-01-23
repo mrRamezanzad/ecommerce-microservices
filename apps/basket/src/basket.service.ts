5f245e44118030e1d1fa91b7598989041af54e98
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-23 10:22:30 +0330

    feat(basket): add API services

--- apps/basket/src/basket.service.ts
+++ apps/basket/src/basket.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { BasketRepository } from './repository/basket.repository';
+import { ShoppingCart } from './entity/shoppingCart.entity';
 
 @Injectable()
 export class BasketService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly basketRepository: BasketRepository) {}
+
+  getBasket(username: string): ShoppingCart | Promise<ShoppingCart> {
+    return this.basketRepository.getBasket(username);
+  }
+
+  updateBasket(basket: ShoppingCart): ShoppingCart | Promise<ShoppingCart> {
+    // TODO: get latest price from discount grpc service
+    return this.basketRepository.updateBasket(basket);
+  }
+
+  deleteBasket(username: string): boolean | Promise<boolean> {
+    return this.basketRepository.deleteBasket(username);
   }
 }
