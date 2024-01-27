face56cc61ea06788846931b4a59c7b37727588e
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-27 16:56:46 +0330

    feat(basket): add grpc client to basket module

--- apps/basket/src/basket.module.ts
+++ apps/basket/src/basket.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { BasketController } from './basket.controller';
 import { BasketService } from './basket.service';
+import { BasketRepository } from './repository/basket.repository';
+import { join } from 'path';
+import { DISCOUNT_PACKAGE_NAME } from '@app/common';
+import { DiscountService } from './grpc/discount.service';
 
 @Module({
+  imports: [
   imports: [],
+    ClientsModule.register([
+      {
+        name: DISCOUNT_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: DISCOUNT_PACKAGE_NAME,
+          protoPath: join(__dirname, '../discount.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [BasketController],
-  providers: [BasketService],
+  providers: [BasketService, BasketRepository, DiscountService],
 })
 export class BasketModule {}
