1d549dafee518bbb8f0b13bf93cf8ee95157773a
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-27 16:58:48 +0330

    feat(basket): complete basket API's

--- apps/basket/src/basket.controller.ts
+++ apps/basket/src/basket.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
 import { BasketService } from './basket.service';
 import { ApiTags } from '@nestjs/swagger';
+import { ShoppingCart } from './entity/shoppingCart.entity';
+import { UpdateBasketDto } from './dtos/updateBasket.dto';
+import { ShoppingCartMapper } from './mapper/shoppingCart.mapper';
 
 @ApiTags('basket')
-@Controller()
+@Controller('basket')
 export class BasketController {
   constructor(private readonly basketService: BasketService) {}
 
-  @Get()
-  getHello(): string {
-    return this.basketService.getHello();
+  @Get(':username')
+  async getBasket(@Param('username') username: string): Promise<ShoppingCart> {
+    const basket = await this.basketService.getBasket(username);
+    return basket ?? new ShoppingCart(username);
+  }
+
+  @Patch()
+  updateBasket(
+    @Body() updateBasketDto: UpdateBasketDto,
+  ): ShoppingCart | Promise<ShoppingCart> {
+    const shoppingCart = ShoppingCartMapper.toDomain(updateBasketDto);
+
+    return this.basketService.updateBasket(shoppingCart);
+  }
+
+  @Delete(':username')
+  deleteBasket(
+    @Param('username') username: string,
+  ): boolean | Promise<boolean> {
+    return this.basketService.deleteBasket(username);
   }
 }
