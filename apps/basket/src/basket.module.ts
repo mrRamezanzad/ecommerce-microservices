411983b0c0f4b5e911108b735471e5a168975802
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-27 16:57:42 +0330

    feat(basket): add redis to basket application

--- apps/basket/src/basket.module.ts
+++ apps/basket/src/basket.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { BasketController } from './basket.controller';
 import { BasketService } from './basket.service';
 import { BasketRepository } from './repository/basket.repository';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { DISCOUNT_PACKAGE_NAME } from '@app/common';
 import { DiscountService } from './grpc/discount.service';
 
 @Module({
   imports: [
-  imports: [],
+    RedisModule.forRoot({
+      type: 'single',
+      url: process.env.REDIS_URL,
+    }),
     ClientsModule.register([
       {
         name: DISCOUNT_PACKAGE_NAME,
