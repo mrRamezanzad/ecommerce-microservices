12088a13ac48e4415a045aa302c6224361a7c2e4
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-01-27 16:53:47 +0330

    feat(basket): add discount effect in updateBasket service

--- apps/basket/src/basket.service.ts
+++ apps/basket/src/basket.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { BasketRepository } from './repository/basket.repository';
 import { ShoppingCart } from './entity/shoppingCart.entity';
+import { DiscountService } from './grpc/discount.service';
 
 @Injectable()
 export class BasketService {
-  constructor(private readonly basketRepository: BasketRepository) {}
+  constructor(
+    private readonly basketRepository: BasketRepository,
+    private readonly discountService: DiscountService,
+  ) {}
 
   getBasket(username: string): ShoppingCart | Promise<ShoppingCart> {
     return this.basketRepository.getBasket(username);
   }
 
-  updateBasket(basket: ShoppingCart): ShoppingCart | Promise<ShoppingCart> {
-    // TODO: get latest price from discount grpc service
+  async updateBasket(basket: ShoppingCart): Promise<ShoppingCart> {
+    for (const item of basket.items) {
+      try {
+        const coupon = await this.discountService.getDiscount(item.productName);
+        item.price -= coupon.amount;
+      } catch (err) {
+        console.log('rpc exception', { err: err.details });
+      }
+    }
+
     return this.basketRepository.updateBasket(basket);
   }
 
