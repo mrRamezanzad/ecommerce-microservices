55732ad4d9fca8078ce99b5723961b902dc48354
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-02-06 15:12:17 +0330

    test(order): add typeorm and mysql configs to project

--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_ADDRESS,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development' && true,
+    }),
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
